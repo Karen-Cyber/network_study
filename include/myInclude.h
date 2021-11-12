59c545382a9eb6965af250c811d276c3aa5459fd
Karen-Cyber <1031307782@qq.com>
2021-11-12 23:40:12 +0800

    first commit

---
+++ include/myInclude.h
@@ -0,0 +1,7 @@
+#ifndef MY_IN_H
+#define MY_IN_H
+
+
+void print_hello();
+
+#endif
---
+++ src/myImplement.c
@@ -0,0 +1,9 @@
+#include <myInclude.h>
+#include <stdio.h>
+
+// do nothing util something is needed
+
+void print_hello()
+{
+    printf("Hello, World\n");
+}
